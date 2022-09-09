57530b2840eb7478adab66e01c08a1973234d1a4
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-09 15:42:26 +0300

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
