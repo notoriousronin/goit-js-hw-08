a05a234720cc218be146d53230430bf4a5a22b11
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-10 00:59:55 +0300

    work on 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const emailEl = document.querySelector('.feedback-form');
+const messageEl = document.querySelector('.feedback-form textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const userData = {};
+
+emailEl.addEventListener('submit', onFormSubmit);
+messageEl.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(e) {
+  const message = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    console.log(savedMessage);
+    messageEl.value = savedMessage;
+  }
+}
