7041b7d1b2c4361b024de84b1efe8f32b740e0c0
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-22 14:34:20 +0300

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,50 @@
 import throttle from 'lodash.throttle';
 
-const emailEl = document.querySelector('.feedback-form');
-const messageEl = document.querySelector('.feedback-form textarea');
+const formEl = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const userData = {
-  message: null,
-  email: null,
-};
+const userData = {};
 
 function userDataStorage(e) {
   userData.message = emailEl.elements.message.value;
   userData.email = emailEl.elements.email.value;
 }
 
-emailEl.addEventListener('submit', onFormSubmit);
-messageEl.addEventListener('input', throttle(onTextareaInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onTextareaInput, 500));
 
 populateTextarea();
+
+function onTextareaInput(e) {
+  userData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
 
 function onFormSubmit(e) {
   e.preventDefault();
 
+  const submitData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(submitData);
+  if (parsedData) {
+    console.log(parsedData);
+  }
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextareaInput(e) {
-  const message = e.target.value;
-
-  localStorage.setItem(STORAGE_KEY, message);
-}
-
 function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-  if (savedMessage) {
-    console.log(savedMessage);
-    messageEl.value = savedMessage;
+  if (localStorage.getItem(STORAGE_KEY)) {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    userData = JSON.parse(savedMessage);
+  }
+  if (userData.email) {
+    input.value = userData.email;
+  }
+  if (userData.message) {
+    textarea.value = userData.message;
   }
 }
